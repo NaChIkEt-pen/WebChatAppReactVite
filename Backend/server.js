904b24c6bf7b08a899d821555832b39407549e00
Nachiket Pensalwar <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-01-16 08:08:31 +0530

    db connected(REST API created)

---
+++ Backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://0.0.0.0:27017/ChatApp');
+
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open',() => console.log('Connected to db'))
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required:true
+  },
+  age: {
+    type: Number,
+    required:true
+  }
+})
+
+const Chats = mongoose.model('chats', schema)
+
+app.use(express.json())
+
+
+
+app.get('/', async (req, res) => {
+  try {
+    const chats = await Chats.find()
+    res.json(chats)
+  } catch {
+    res.status(500).json({message : err.message})
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server Started')
+})
